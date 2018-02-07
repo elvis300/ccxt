a61cf96aa86ffc9899b34bb43dc4b3df5c3ecc71
elvis300 <35616354+elvis300@users.noreply.github.com>
2018-02-08 00:06:48 +0200

    Update onPiV1.py

--- onPiV1.py
+++ onPiV1.py
@@ -15,19 +15,19 @@ raw = padding
 device = sh1106(port=1, address=0x3C)
 bottom = device.height - padding - 1 
 font = ImageFont.load_default()
-binance = ccxt.binance()
+exchange = ccxt.binance()
 
-def formated_ticker2(yo, pair):
+def formated_ticker2(exchange, pair):
     print "formated_ticker with ",pair
     ticker = binance.fetch_ticker(pair)
     for key, val in ticker[pair].iteritems():
         if key == "last":
             return val
 
-def printer(draw,yo, pair):
+def printer(draw,exchange, pair):
 	global raw
 	
-	lastPrice = formated_ticker2(yo,pair)  
+	lastPrice = formated_ticker2(exchange,pair)  
 	draw.text((x, raw), pair,  font=font, fill=255)
 	draw.text((x+spacing , raw), str(lastPrice), font=font, fill=255)
 	raw = raw + 10
@@ -40,10 +40,10 @@ def main():
   global raw
   while True:
 	with canvas(device) as draw:
-		printer(draw,yo,"btc/usd")
-		printer(draw,yo,"dcr/btc")
-		printer(draw,yo,"etc/btc")
-		printer(draw,yo,"cme/btc")
+		printer(draw,exchange,"btc/usd")
+		printer(draw,exchange,"dcr/btc")
+		printer(draw,exchange,"etc/btc")
+		printer(draw,exchange,"cme/btc")
 	print "sleep 5 seconds"
 	time.sleep(5)
 	#del draw
